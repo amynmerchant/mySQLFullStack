01c86243f3196feb7762d934de735421fb87e610
AM <amyn_merchant2808@hotmail.com>
2018-09-07 00:52:17 -0400

    adding some initial files

---
+++ server.js
@@ -0,0 +1,38 @@
+//Dependencies
+var express = require("express");
+var bodyParser = require("body-parser");
+var methodOverride = require("method-override")
+var exphbs = require("express-handlebars");
+var sql = require("mysql")
+
+var app = express();
+
+//Static content for the app
+app.use(express.static(__dirname + "/public"));
+
+// Body Parser
+app.use(bodyParser.urlencoded({
+	extended: false
+}))
+
+// override with POST having ?_method=DELETE
+app.use(methodOverride("_method"))
+
+//Add Handlebars to express
+app.engine("handlebars", exphbs({
+    defaultLayout: "main"
+}));
+app.set("view engine", "handlebars");
+
+//Routing
+var routes = require("./controllers/burgers_controller.js");
+app.use("/", routes);
+app.use("/update", routes);
+app.use("/create", routes);
+
+//Port Listener
+var PORT = 3000;
+app.listen(PORT, function(){
+	console.log("Listening on port " + PORT)
+});
+
