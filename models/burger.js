481232eb1430a07804e969064274f573a391633a
AM <amyn_merchant2808@hotmail.com>
2018-09-07 22:42:12 -0400

    uploading final files

--- models/burger.js
+++ models/burger.js
@@ -1,21 +1,23 @@
-module.exports = function(sequelize, DataTypes) {
-    var Burger = sequelize.define("Burger", {
-        burger_name: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        devoured: {
-            type: DataTypes.BOOLEAN,
-            defaultValue: false
-        }
-    }, {
-        paranoid: true,
-        tableName: "burgers",
-        classMethods: {
-            associate: function(models) {
-                Burger.belongsTo(models.Customer);
-            }
-        }
-    });
-    return Burger;
+var orm = require("../config/orm.js");
+
+
+var burger = {
+	selectAll: function(cb) {
+		orm.selectAll('burgers', function(res) {
+			cb(res);
+		});
+	},
+	insertOne: function(cols, vals, cb) {
+		orm.insertOne('burgers', cols, vals, function(res) {
+			cb(res);
+		});
+	},
+	updateOne: function(objColVals, condition, cb) {
+		orm.updateOne('burgers', objColVals, condition, function(res) {
+			cb(res);
+		});
+	}
 };
+
+
+module.exports = burger;
--- server.js
+++ server.js
@@ -1,38 +1,38 @@
 //Dependencies
-var express = require("express");
-var bodyParser = require("body-parser");
-var methodOverride = require("method-override")
-var exphbs = require("express-handlebars");
-var sql = require("mysql")
+var express = require('express');
+var methodOverride = require('method-override');
+var bodyParser = require('body-parser');
 
+// Create an instance of the express app.
 var app = express();
 
-//Static content for the app
-app.use(express.static(__dirname + "/public"));
+// set port to 3000 or whatever heroku (deployment site) sets it to
+var PORT = process.env.PORT || 3000;
 
-// Body Parser
-app.use(bodyParser.urlencoded({
-	extended: false
-}))
+// express middleware needed for serving static files. For more details
+// see here: http://expressjs.com/en/starter/static-files.html
+app.use(express.static(__dirname + '/public'));
 
-// override with POST having ?_method=DELETE
-app.use(methodOverride("_method"))
+/// bodyparsers 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.json({ type: 'application/*+json' }));
+app.use(bodyParser.raw({ type: 'application/vnd.custom-type' }));
+app.use(bodyParser.text({ type: 'text/html' }));
 
-//Add Handlebars to express
-app.engine("handlebars", exphbs({
-    defaultLayout: "main"
-}));
+// override with POST having ?_method=DELETE or PUT
+app.use(methodOverride('_method'));
+
+// Set Handlebars as the default templating engine.
+var exphbs = require('express-handlebars');
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-//Routing
-var routes = require("./controllers/burgers_controller.js");
-app.use("/", routes);
-app.use("/update", routes);
-app.use("/create", routes);
+// now import the routes
+var routes = require('./controllers/burgers_controller.js');
+app.use('/', routes);
 
-//Port Listener
-var PORT = 3000;
-app.listen(PORT, function(){
-	console.log("Listening on port " + PORT)
+// Initiate the listener.
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
 });
-
