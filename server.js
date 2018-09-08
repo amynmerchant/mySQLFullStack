ebe31fca2c31c032088e9d905de2dad824c64bb1
AM <amyn_merchant2808@hotmail.com>
2018-09-07 22:51:14 -0400

    changes to server

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.static(__dirname + '/public'));
 /// bodyparsers 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(bodyParser.json({ type: 'application/*+json' }));
+//app.use(bodyParser.json({ type: 'application/*+json' }));
 app.use(bodyParser.raw({ type: 'application/vnd.custom-type' }));
 app.use(bodyParser.text({ type: 'text/html' }));
 
